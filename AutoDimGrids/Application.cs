6e89a6956e75b76faa4f5698e829c709a33d7746
GastonWkLaptop <gastonbalparda@gmail.com>
2021-01-22 17:55:13 -0300

    Initial commit

---
+++ AutoDimGrids/Application.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.IO;
+using System.Reflection;
+
+namespace AutoDimGrids
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId(GlobalVars.APP_GUID)]
+    public class ThisApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication uiApp)
+        {
+            #region GAS ADDIN BOILERPLATE
+
+            // Finds and creates the tab, finds and creates the panel
+
+            
+
+            string ThisDllPath = Assembly.GetExecutingAssembly().Location;
+            Assembly ThisAssembly = Assembly.GetExecutingAssembly();
+
+            // Assembly that contains the invoke method
+            String exeConfigPath = Path.GetDirectoryName(ThisDllPath) + "\\AutoDimGrids.dll";
+
+            RibbonPanel DefaultPanel = null;
+
+            // First try to create the tab
+            try
+            {
+                uiApp.CreateRibbonTab(GlobalVars.TAB_NAME);
+            }
+            catch (Autodesk.Revit.Exceptions.ArgumentException)
+            {
+                // Tab is already created
+            }
+
+
+            // Then create the panel
+            try
+            {
+                DefaultPanel = uiApp.CreateRibbonPanel(GlobalVars.TAB_NAME, GlobalVars.PANEL_NAME);
+            }
+
+            catch (Autodesk.Revit.Exceptions.ArgumentException)
+            {
+                DefaultPanel = uiApp.GetRibbonPanels(GlobalVars.TAB_NAME).FirstOrDefault(n => n.Name.Equals(GlobalVars.PANEL_NAME, StringComparison.InvariantCulture));
+            }
+            #endregion
+
+
+            try
+            {
+
+                string AutoDimGridsName = "Auto Dimension Grids";
+                PushButtonData AutoDimGridsData = new PushButtonData(AutoDimGridsName, AutoDimGridsName, exeConfigPath, "AutoDimGrids.ThisCommand"); // Invoke class, pushbutton data
+                AutoDimGridsData.LargeImage = Utils.RetriveImage("AutoDimGrids.Resources.GridsIcons32x32.ico"); // Pushbutton image
+                AutoDimGridsData.ToolTip = "Automatically turn on or off grid bubbles correctly";
+                PushButton SmartGridsButton = DefaultPanel.AddItem(AutoDimGridsData) as PushButton;
+
+                return Result.Succeeded;
+            }
+
+            catch (Exception ex)
+            {
+                Utils.CatchDialog(ex);
+                return Result.Failed;
+            }
+        }
+
+
+
+    }
+}
---
+++ AutoDimGrids/Command.cs
@@ -0,0 +1,112 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDimGrids
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.DB.Macros.AddInId(GlobalVars.CMD_GUID)]
+    class ThisCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            try
+            {
+
+                ReferenceArray GridsRefRow = new ReferenceArray();
+                ReferenceArray GridsRefCol = new ReferenceArray();
+
+                XYZ TopPoint = null;
+                XYZ LeftPoint = null;
+
+
+                foreach (ElementId elemId in uidoc.Selection.GetElementIds())
+                {
+                    Element SelectedElem = doc.GetElement(elemId);
+                    if (SelectedElem is DatumPlane)
+                    {
+                        DatumPlane xGrid = SelectedElem as DatumPlane;
+
+                        IList<Curve> CurvesInGrid = xGrid.GetCurvesInView(DatumExtentType.ViewSpecific, uidoc.ActiveView);
+
+                        Curve cv = CurvesInGrid.First(); // only one segment grids allowed
+
+                        XYZ cvStart = cv.GetEndPoint(0);
+                        XYZ cvEnd = cv.GetEndPoint(1);
+
+                        if (Convert.ToInt32(cvStart.Y) == Convert.ToInt32(cvEnd.Y)) // then its a horizontal line, need to round the double bc the comparison will never work
+                        {
+                            GridsRefRow.Append(new Reference(xGrid));
+
+
+                            if (LeftPoint is null && cvStart.X < cvEnd.X) // drawn left to right, keep start    X--------->
+                            {
+                                LeftPoint = cvStart;
+                            }
+
+                            else if (LeftPoint is null && cvStart.X > cvEnd.X) // drawn right to left, keep end    <---------X
+                            {
+                                LeftPoint = cvEnd;
+                            }
+                        }
+
+                        else if (Convert.ToInt32(cvStart.X) == Convert.ToInt32(cvEnd.X)) // then its a vertical
+                        {
+                            GridsRefCol.Append(new Reference(xGrid));
+
+                            if (TopPoint is null && cvStart.Y < cvEnd.Y) // drawn bottom to top, keep end
+                            {
+                                TopPoint = cvEnd;
+                            }
+
+                            else if (TopPoint is null && cvStart.Y > cvEnd.Y) // drawn top to bottom, keep start
+                            {
+                                TopPoint = cvStart;
+                            }
+                        }
+
+
+
+                    }
+                }
+
+                using (Transaction t = new Transaction(doc, "Auto dimension grids"))
+                {
+                    t.Start();
+
+                    if (GridsRefCol.Size >= 2)
+                    {
+                        XYZ SecondTopPoint = new XYZ(TopPoint.X + 10d, TopPoint.Y, TopPoint.Z);
+
+                        Line lineC = Line.CreateBound(TopPoint, SecondTopPoint);
+                        doc.Create.NewDimension(uidoc.ActiveView, lineC, GridsRefCol);
+                    }
+
+                    if (GridsRefRow.Size >= 2)
+                    {
+                        XYZ SecondLeftPoint = new XYZ(LeftPoint.X, LeftPoint.Y + 10d, LeftPoint.Z);
+
+                        Line lineR = Line.CreateBound(LeftPoint, SecondLeftPoint);
+                        doc.Create.NewDimension(uidoc.ActiveView, lineR, GridsRefRow);
+                    }
+
+                    t.Commit();
+                }
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                Utils.CatchDialog(ex);
+                return Result.Failed;
+            }
+        }
+    }
+}
